bb6ff8540088a04bc9ecc99e1c697203701518b8
Brian Oley <brian.oley@gmail.com>
2014-09-04 17:16:25 -0400

    Checking in old code...

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,23 +10,31 @@
 angular.module('acroGameApp')
   .controller('MainCtrl', function ($scope) {
     
+    $scope.showTut = !!!localStorage.getItem('showTut', true);
+
     $scope.list = [];
 
     $scope.star = ['star', 'reverse star'];
     $scope.direction = [' on left', ' on right'];
     $scope.throne = ['throne', 'reverse throne'];
 
-    $scope.generate = function () {
-        $scope.list[0] = 'From ' + $scope.star[$scope.rand()];
-        $scope.list[1] = ' to ' + $scope.throne[$scope.rand()] + $scope.direction[$scope.rand()];
-        $scope.list[2] = ' to ' + ($scope.list[0].split(' ')[1] === $scope.star[0] ? $scope.star[1] : $scope.star[0]) + $scope.direction[$scope.rand()];
-        $scope.list[3] = ' to ' + ($scope.list[1].split(' ')[2] === $scope.throne[0] ? $scope.throne[1] : $scope.throne[0]) + $scope.direction[$scope.rand()] + '.';
+    $scope.generate = function (init) {
+        
+        if (!init && $scope.showTut === true) {
+            $scope.showTut = false;
+            localStorage.setItem('showTut', false);
+        }
+        
+        $scope.list[0] = $scope.star[$scope.rand()];
+        $scope.list[1] = $scope.throne[$scope.rand()] + $scope.direction[$scope.rand()];
+        $scope.list[2] = ($scope.list[0] === $scope.star[0] ? $scope.star[1] : $scope.star[0]) + $scope.direction[$scope.rand()];
+        $scope.list[3] = ($scope.list[1].split(' ')[0] === $scope.throne[0] ? $scope.throne[1] : $scope.throne[0]) + $scope.direction[$scope.rand()];
     };
 
     $scope.rand = function () {
         return Math.round(Math.random());
     };
 
-    $scope.generate();
+    $scope.generate(true);
 
   });
